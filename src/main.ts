d5fd8993d362a5a5d968d306e08c1ae84605b2b4
Donald Oakes <donald.h.oakes@gmail.com>
2020-10-13 17:12:33 -0600

    flow rendering in flowbee

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,37 @@
 import * as flowbee from 'flowbee';
 import { Descriptors } from './descriptors';
-import { Flows } from './flows';
 
 window.addEventListener('load', async () => {
     const base = '';
     const dark = false;
+    const readonly = false;
 
     const descriptors = await Descriptors.getDescriptors(base);
 
     const flowTreeElement = document.getElementById('flow-tree') as HTMLDivElement;
-
-    await new Flows(base).render(dark);
+    const root = await (await fetch(`${base}/flows`)).json();
+    const flowTreeOptions = dark ? flowbee.DefaultOptions.flowTree.dark : flowbee.DefaultOptions.flowTree.light;
+    const flowTree = new flowbee.FlowTree(flowTreeElement, flowTreeOptions);
+    flowTree.render(root);
+    flowTree.onFlowSelect(async selectEvent => {
+        const canvasElement = document.getElementById('diagram-canvas') as HTMLCanvasElement;
+        const text = await (await fetch(selectEvent.path)).text();
+        const diagramOptions = dark ? flowbee.DefaultOptions.diagram.dark : flowbee.DefaultOptions.diagram.light;
+        diagramOptions.iconBase = `${base}/icons`;
+        console.debug(`rendering ${selectEvent.path} to canvas`);
+        const flow = new flowbee.FlowDiagram(canvasElement, diagramOptions, descriptors, readonly);
+        const instance = undefined;
+        const step: string | undefined = undefined;
+        const animate = false;
+        const instanceEdit = false;
+        const data = undefined;
+        flow.render(text, selectEvent.name, instance, step, animate, instanceEdit, data);
+    });
 
     const toolboxElement = document.getElementById('flow-toolbox') as HTMLDivElement;
     const toolboxOptions = dark ? flowbee.DefaultOptions.toolbox.dark : flowbee.DefaultOptions.toolbox.light;
     toolboxOptions.iconBase = `${base}/icons`;
-    await new flowbee.Toolbox(
-        toolboxElement,
-        toolboxOptions,
-        descriptors
-    ).render();
+    await new flowbee.Toolbox(toolboxElement, toolboxOptions).render(descriptors);
 
     // avoid resize when collapse
     flowTreeElement.style.width = flowTreeElement.style.minWidth = flowTreeElement.style.maxWidth = (flowTreeElement.offsetWidth - 2) + 'px';
