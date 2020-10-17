aca1a1d53f746b7f5507b00cb7f2682e461a9ea2
Donald Oakes <donald.h.oakes@gmail.com>
2020-10-16 22:48:26 -0600

    api changes

--- src/main.ts
+++ src/main.ts
@@ -25,20 +25,22 @@ window.addEventListener('load', async () => {
     flowTree.render(theme, root);
     flowTree.onFlowSelect(async (selectEvent: flowbee.FlowTreeSelectEvent) => {
         const canvasElement = document.getElementById('diagram-canvas') as HTMLCanvasElement;
-        canvasElement.parentElement.className = `flowbee-diagram-${theme} diagram`;
         const text = await (await fetch(selectEvent.path)).text();
         const diagramOptions = { iconBase: `${base}/icons` };
         console.debug(`rendering ${selectEvent.path} to canvas`);
         flowDiagram = new flowbee.FlowDiagram(canvasElement, diagramOptions, descriptors);
         flowDiagram.readonly = readonly;
-        flowDiagram.render(theme, text, selectEvent.name, instance, step, animate, editInstanceId);
+        flowDiagram.instance = instance;
+        flowDiagram.step = step;
+        flowDiagram.editInstanceId = editInstanceId;
+        flowDiagram.render(theme, text, selectEvent.name, { grid: { visibility: 'visible' } }, animate);
     });
 
     const flowDiagramElement = document.getElementById('flow-diagram') as HTMLDivElement;
     flowDiagramElement.style.backgroundColor = theme === 'dark' ? '#1e1e1e' : '#ffffff';
     const toolboxElement = document.getElementById('flow-toolbox') as HTMLDivElement;
     const toolboxOptions = { iconBase: `${base}/icons` };
-    const toolbox = await new flowbee.Toolbox(toolboxElement, toolboxOptions);
+    const toolbox = new flowbee.Toolbox(toolboxElement, toolboxOptions);
     toolbox.render(theme, descriptors);
 
     new Splitters(flowTreeElement, toolboxElement);
@@ -50,7 +52,7 @@ window.addEventListener('load', async () => {
         toolbox.render(theme, descriptors);
         flowDiagramElement.style.backgroundColor = theme === 'dark' ? '#1e1e1e' : '#ffffff';
         if (flowDiagram) {
-            flowDiagram.render(theme, flowDiagram.flow, flowDiagram.name, instance, step, animate, editInstanceId);
+            flowDiagram.render(theme, flowDiagram.flow, flowDiagram.flow.name, { grid: { visibility: 'visible' } }, animate);
         }
     });
 
